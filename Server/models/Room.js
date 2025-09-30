ede81e4571555804f64fbce5357cfc90cf176049
vineet <Mastergenos228@gmail.com>
2025-09-30 15:02:52 +0530

    Adding the rooms Schema

---
+++ Server/models/Room.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+  roomId: { type: String, required: true, unique: true },
+  topic: { type: String, required: true, index: true },
+  title: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Room', roomSchema);
