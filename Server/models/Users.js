86511b8d6aea64630b30df9a26d28df3f9dfd9e6
vineet <Mastergenos228@gmail.com>
2025-10-08 19:08:29 +0530

    Adding Description Endpoints

--- Server/models/Users.js
+++ Server/models/Users.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   email: { type: String, required: true },
-  phone: { type: String, required: true },
+  phone: { type: String},
   rank : { type : Number, default:99999} ,
   created_at: { type: Date, default: Date.now },
   user_image : {type:String, default: ""},
