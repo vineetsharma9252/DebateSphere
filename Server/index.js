570ef01f0413093210ce02d0dd13ff692c26ddf6
vineet <Mastergenos228@gmail.com>
2025-10-08 19:30:30 +0530

    Adding Description Endpoints

--- Server/index.js
+++ Server/index.js
@@ -219,6 +219,7 @@ app.post("/api/get_desc" , async (req, res)=>{
         const username = req.body.username ;
         try {
         const user = User.findOne({username});
+        console.log(user) ;
         if (!user){
             return res.status(404).json({error: "User not found"})
         }
