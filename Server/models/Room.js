400521b6e4a89572c0241a322459fec817852482
vineet <Mastergenos228@gmail.com>
2025-10-15 10:30:21 +0530

    Redefining the Rooms Schema

--- Server/models/Room.js
+++ Server/models/Room.js
@@ -5,6 +5,8 @@ const roomSchema = new mongoose.Schema({
   topic: { type: String, required: true, index: true },
   title: { type: String, required: true },
   desc : { type : String , default:"" } ,
+  isActive : {type:Boolean , default:true} ,
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
